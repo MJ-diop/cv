314ccc2201c5c02dfb20ba557cd51b38d4efd707
Mbaye <m_j_d@mjd.com>
2018-12-06 04:29:53 +0000

    reglage de la compatilité avec firfox

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.navbar a, footer a').on('click', function (event) {
         event.preventDefault();
         let hash = this.hash;
-        $('body').animate({
+        $('body,html').animate({
             scrollTop: $(hash).offset().top
         }, 900, function () {
             window.location.hash = hash;
